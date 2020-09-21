b28779011a4fcffd2a50df3b11f9815a7c48c3ab
Jakit <jakit@Jakits-MacBook-Pro.local>
2020-09-21 18:58:37 +0530

    Made some changes based on shouki's changes

--- app.js
+++ app.js
@@ -3,47 +3,98 @@ let fields = require("./fields.json");
 //console.log(fields);
 
 // Requiring Wires Json file 
-let wireDtObj = require("./wire.json");
-//console.log(wireDtObj);
+let wire = require("./wire.json");
+//console.log(wire);
+
+let errorMsg = "";
 
 for(var i = 0; i < fields.length; i++) {
     var obj = fields[i];
-    //console.log(obj.tag);
+    console.log(obj.tag);
     let elementArr = obj.elements;
+    console.log("element.len = "+ elementArr.length);
+    
     for(var j = 0; j < elementArr.length; j++) {
+        
         var objElement = elementArr[j];
-        //console.log(objElement.name);
-        //console.log(wireDtObj[objElement.name]);
-        let val = wireDtObj[objElement.name];
-        let errorMsg = "";
-        let err1 = checkLength(obj.tag, objElement, val);
-        if(err1 !== null){
-            errorMsg = errorMsg+err1+" ";
+        console.log(objElement.name);
+        let val = wire[objElement.name];
+        console.log("val =" + val);
+        
+        if(objElement.mandatory == 0){
+            let err1 = checkOptional(obj.tag, objElement, val);
+            if(err1 !== null){
+                errorMsg = errorMsg+err1+" ";
+            }
+        }
+        else if(objElement.mandatory == 1){
+            let err1 = checkMandatory(obj.tag, objElement, val);
+            if(err1 !== null){
+                errorMsg = errorMsg+err1+" ";
+            }
+        }
+        else if(objElement.mandatory == 2){
+            let err1 = checkSpecial(obj.tag, objElement, val);
+            if(err1 !== null){
+                errorMsg = errorMsg+err1+" ";
+            }
+        }
+        else {
+            if(err1 !== null){
+                errorMsg = errorMsg+obj.tag+":not valid mandatory value ";
+            }
         }
         
-        let err2 = checkMandatory(obj.tag, objElement, val);
-        if(err2 !== null){
-            errorMsg = errorMsg+err2+" ";
-        }
-        console.log(errorMsg);
-        console.log("\n");
     }
 }
+
+console.log("Error:" + errorMsg);
+console.log("\n");
+
 function checkMandatory(tag, objElement, val){
+    console.log("checkMandatory: tag =" + tag + " objElement=" + JSON.stringify(objElement) + " val=" + val);
+    
     let err = null;
-    if(objElement.mandatory == 1){
-        if(val == null || val == ""){
+   
+    if( typeof val == 'undefined' || val === null || val === ""){
             err = tag+":"+objElement.name+": value is mandatory;";
+    }
+    // check if val exist in value array
+    //
+    else if(objElement.value !== "")
+    {
+        var n = objElement.value.includes(val);
+        //console.log("checkMandatory exist : " + n);
+        if(n === false)
+        {
+            err = tag+":"+objElement.name+": value " + val + " not in " + objElement.value;
+        }
+    }
+    
+    return err;
+}
+
+function checkOptional(tag, objElement, val){
+    let err = null;
+    if(typeof val !== 'undefined'){
+        if(val !== null && val.length > objElement.length){
+            err = tag+":"+objElement.name+": value too long;";
         }
     }
     return err;
 }
 
-function checkLength(tag, objElement, val){
+function checkSpecial(tag, objElement, val){
+    console.log("checkSpecial: tag =" + tag + " objElement=" + JSON.stringify(objElement) + " val=" + val);
+    
     let err = null;
-    if(val!==null && typeof val !== 'undefined'){
-        if(val.length !== objElement.length){
-            err = tag+":"+objElement.name+": has not correct length;";
+    if(objElement.desc !== "" && objElement.desc !== null){
+        let rulesArr = objElement.desc.split(";");
+        if(rulesArr.length>0){
+            for(let k=0; k<rulesArr.length; k++){
+                let rule = rulesArr[k];
+                console.log(rule);
+            }
         }
     }
     return err;
